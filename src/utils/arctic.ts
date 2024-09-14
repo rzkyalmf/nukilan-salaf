92523992a0baf42d3b41887766de69c4facf5ab9
Rizky Alam <sketsacartoon@gmail.com>
2024-09-14 17:29:07 +0700

    update env

--- src/utils/arctic.ts
+++ src/utils/arctic.ts
@@ -1,3 +1,7 @@
 import { Google } from "arctic";
 
-export const google = new Google(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URI);
+const getRedirectUri = () => {
+  return process.env.NODE_ENV === "production" ? process.env.GOOGLE_REDIRECT_URI_PROD : process.env.GOOGLE_REDIRECT_URI_DEV;
+};
+
+export const google = new Google(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, getRedirectUri());
